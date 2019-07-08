027eccbe090ca31b45b158b50af73edd93fbb656
b20bharath <52134126+b20bharath@users.noreply.github.com>
2019-07-08 13:54:46 +0530

    Add files via upload

---
+++ python-data structures basic/lists.py
@@ -0,0 +1,12 @@
+# Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    line = line.rstrip()
+    text = line.split()
+    for word in text:
+        if word not in lst:
+            lst.append(word)
+lst.sort()
+print(lst)
