b82fa95e3a7fea2ab4c864439eed3d5264c07acd
b20bharath <52134126+b20bharath@users.noreply.github.com>
2020-03-18 12:04:05 -0500

    Add files via upload

---
+++ python-access_web_data/following_links_urllib.py
@@ -0,0 +1,21 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter URL: ')
+count = input('Enter count: ')
+position = input('Enter position: ')
+i = 0
+while i < int(count):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    url = tags[int(position)-1].get('href', None)
+    i = i+1
+words = url.split('_')
+final = words[2].split('.')
+print(final[0])
